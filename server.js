9d7623781622652b2cbfaeafea2f4a5ad35e5b59
Shumax <maxcastro.mv@gmail.com>
2024-01-14 22:15:14 -0300

    feat: client delivery

--- server.js
+++ server.js
@@ -31,15 +31,22 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', async (socket) => {
-  socket.on('chat message', async (msg) => {
+  socket.on('chat message', async (msg, clientOffset, callback) => {
     let result
     try {
-      result = await db.run('INSERT INTO messages (content) VALUES (?)', msg)
+      result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset)
     } catch (err) {
+      if (e.errno === 19 /* SQLITE_CONSTRAINT */ ) {
+        // the message was already inserted, so we notify the client
+        callback();
+      } else {
+        // nothing to do, just let the client retry
+      }
       return 
     }
     io.emit('chat message', msg, result.lastID);
     console.log('message: ' + msg);
+    callback()
   });
 
   if (!socket.recovered) {
