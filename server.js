9a5a4be87195c8e15b7b060ee10f845484aa2e8b
Shumax <maxcastro.mv@gmail.com>
2024-01-14 22:04:08 -0300

    feat: server delivery with sqlite

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+const db = await open({
+  filename: 'chat.db',
+  driver: sqlite3.Database
+});
+
+// create our 'messages' table (you can ignore the 'client_offset' column for now)
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`);
 
 const app = express();
 const server = createServer(app);
@@ -14,11 +30,33 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'public/index.html'));
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+io.on('connection', async (socket) => {
+  socket.on('chat message', async (msg) => {
+    let result
+    try {
+      result = await db.run('INSERT INTO messages (content) VALUES (?)', msg)
+    } catch (err) {
+      return 
+    }
+    io.emit('chat message', msg, result.lastID);
     console.log('message: ' + msg);
   });
+
+  if (!socket.recovered) {
+    // if the connection state recovery was not successful
+    try {
+      await db.each('SELECT id, content FROM messages WHERE id > ?',
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit('chat message', row.content, row.id);
+        }
+      )
+    } catch (e) {
+      // something went wrong
+      console.log('Has Error: ' + e)
+    }
+  }
+
 })
 
 server.listen(3000, () => {
