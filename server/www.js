57529b749bb00dddf2912fc2b9a9f7049437fc73
Max Castro <maxcastro@lance.com.br>
2024-09-10 18:56:19 -0300

    feat: backend websocket

--- server/www.js
+++ server/www.js
@@ -1,11 +1,33 @@
 import express from 'express'
-import { createServer } from 'node:http';
+import { createServer } from 'node:http'
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+import mongoose from 'mongoose';
+
+import socketConnect from './src/infra/websocket/socket.connect.js'
+import mongooseConnect from './src/infra/db/mongo.config.js'
 
 const app = express();
 const server = createServer(app);
 
 const port = process.env.PORT || 3030;
 
-server.listen(port, () => {
-  console.log('server running at http://localhost:3030');
-});
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// express.static('frontend/index.html') 
+
+app.get('/room', (req, res) => { res.sendFile(join(__dirname, '../frontend/index.html')) })
+
+mongooseConnect.then(() => {
+  console.log('MongoDB connected')
+  
+  server.listen(port, () => {
+    console.log('Server is running at http://localhost:3030');
+  })
+
+  socketConnect(server)
+
+}).catch(err => {
+  console.log('MongoDB connection error', err)
+})
+
